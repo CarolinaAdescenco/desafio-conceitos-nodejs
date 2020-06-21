34fd56bbeb9b2a94a26ba3c816f5265507752de8
CarolinaAdescenco <carolinaadescencofernandes@hotmail.com>
2020-06-21 19:26:35 -0300

    Challenge Nodejs

---
+++ src/app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs, likes } = request.body;
+  const repo = { id: uuid(), title, url, techs, likes: (likes == null ? 0 : likes )};
+  repositories.push(repo);
+  return response.json(repo);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if(repoIndex < 0){
+    return response.status(400).json({ error: "Repository not found."})
+  }
+
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories[repoIndex] = repo
+
+  return response.json(repo)
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if(repoIndex < 0){
+    return response.status(400).json({ error: "Repository not found."})
+  }
+
+  repositories.splice(repoIndex, 1)
+
+  return response.status(204).send()
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if(repoIndex < 0){
+    return response.status(400).json({ error: "Repository not found."})
+  }
+
+  repositories[repoIndex].likes++;
+  response.json(repositories[repoIndex])
+
+
+});
+
+module.exports = app;
